fe2b45619b91f7672702813597dec69b2c31dc3c
haroldo gomes <moraesgomes@gmail.com>
2023-09-26 21:57:28 -0300

    first commit

---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeApiService } from '../../service/poke-api.service';
+import { forkJoin } from 'rxjs';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent  implements OnInit{
+
+  private urlPokemon:string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName:string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon:any;
+
+  constructor(
+
+    private activeRoute :ActivatedRoute,
+    private pokeApiService:PokeApiService
+  ){}
+
+  ngOnInit(): void {
+      this.getPokemon;
+  }
+
+   get getPokemon(){
+
+      const id =this.activeRoute.snapshot.params['id'];
+      const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+      const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+      return  forkJoin ([pokemon,name]).subscribe(
+        res=> {
+
+          this.pokemon = res;
+        }
+      )
+
+   }
+
+}
